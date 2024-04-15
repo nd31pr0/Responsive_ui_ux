e543b6604c37de0e755c961243a6c969299fbef7
Ndzi Nfor <ndzinfor@gmail.com>
2024-04-15 11:47:36 +0100

    complete navbar according to figma design

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <div>
-      <h1>hello from the index page</h1>
+      <NavBar />
     </div>
   );
 }
