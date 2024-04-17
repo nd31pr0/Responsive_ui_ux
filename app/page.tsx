e4770127abc0f85be18c1db1d22b3fe7cbaf2938
Ndzi Nfor <ndzinfor@gmail.com>
2024-04-17 07:15:54 +0100

    create Faq component and link to page compo

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Faq from "./components/Faq";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <div className="px-[20px] lg:container lg:px-20 mx-auto">
         <Features />
       </div>
+      <Faq />
     </div>
   );
 }
